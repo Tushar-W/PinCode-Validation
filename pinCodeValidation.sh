bd18b2476bd07529391352d9a3ee599642188ee3
Tushar-W <twankhede95@gmail.com>
2020-04-12 16:40:26 +0530

    ADDED:validation of space betn pincode number after 3 digit

--- pinCodeValidation.sh
+++ pinCodeValidation.sh
@@ -4,10 +4,11 @@ shopt -s extglob #turn on extended globbing
 echo "Welcome To Pin Code Validation"
 #PATTERN
 PINCODE_PATTERN="^[0-9]{6}$"
+PINCODE_SPACE_PATTERN="^([0-9]{3}[ ]?[0-9]{3})$"
 
 #check given pincode validate or not
 function checkPattern() {
-	if [[ $pincode =~ $PINCODE_PATTERN ]];
+	if [[ $1 =~ $2 ]];
 	then
 		echo "PinCode Is Valid"
 	else
@@ -17,11 +18,14 @@ function checkPattern() {
 
 #read pincode from user
 read -p "Enter PinCode:" pincode
-checkPattern
+checkPattern $pincode $PINCODE_PATTERN
 #restrict the pincode from taking alphabet or special char at begin
-read -p "Enter PinCode:" pincode
-checkPattern
+pincode=A400088
+checkPattern $pincode $PINCODE_PATTERN
 #restrict the pincode from taking alphabet or special char at end
+pincode=400088B
+checkPattern $pincode $PINCODE_PATTERN
+#space between pincode number after three digit
 read -p "Enter PinCode:" pincode
-checkPattern
+checkPattern "$pincode" "$PINCODE_SPACE_PATTERN"
 
